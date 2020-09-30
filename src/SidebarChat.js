142e54c0e8328357d03c637a1e79b18d826ec425
okvarsh <varshvilas@gmail.com>
2020-09-30 16:52:27 +0530

    first commit

---
+++ src/SidebarChat.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import'./SidebarChat.css';
+
+function SidebarChat() {
+    return (
+        <div className="sidebarChat">
+            <h1>
+                Hey
+            </h1>
+        </div>
+    )
+} 
+
+
+export default SidebarChat
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB4hLnY2ZUwJzKDDY8sgBPCcROjiB6zSPg",
+    authDomain: "amaze-chat.firebaseapp.com",
+    databaseURL: "https://amaze-chat.firebaseio.com",
+    projectId: "amaze-chat",
+    storageBucket: "amaze-chat.appspot.com",
+    messagingSenderId: "514306908765",
+    appId: "1:514306908765:web:0955c6ac9b94682b2723b4",
+    measurementId: "G-CY8GJ35VDK"
+  };
